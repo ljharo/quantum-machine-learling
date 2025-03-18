3f65134b7fce234c7689fcec74b6a97cbb3117ea
Tu Nombre <you@example.com>
2025-03-18 13:53:03 -0400

    first commit

---
+++ scripts/iris.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+import pickle
+
+IRIS_FILENAME = "dataset/iris.dataset.pickle"
+
+try:
+    with open(IRIS_FILENAME, "rb") as f:
+        dataset = pickle.load(f)
+except:
+    dataset = datasets.load_iris()
+    with open(IRIS_FILENAME, "wb") as f:
+        pickle.dump(dataset, f) 
